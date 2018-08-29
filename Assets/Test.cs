aad01e6956a9b0d82c86b6a94b9124fe12f73ecd
NoesAlt <noesofscarlet@gmail.com>
2018-08-29 17:12:36 +0900

    ScriptTestプロジェクト　初めてのコミット
    Lesson4の課題のソースコードを作ったものになります。

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+  
+ 
+
+    void Start () {
+        int[] array = { 8, 94, 49, 30, 20 };
+
+        int i, j;
+
+        for (i = 0; i <= 4; i++)
+            Debug.Log(array[i]);
+
+        for(j = 4; j >= 0; j--)
+        {
+            Debug.Log(array[j]);
+        }
+
+       
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
---
+++ Assets/Test2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Boss
+{
+    private int mp = 53;    //MP値
+
+    //魔法攻撃をする関数
+    public int Magic(int magic)
+    {
+        
+       
+            if (mp >= magic)
+            {
+                mp -= magic;
+
+                Debug.Log("魔法攻撃をした。残りMPは。"+mp);
+            }
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない。");
+            }
+           
+        
+        return mp;
+    }
+
+
+}
+
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+
+       
+	void Start () {
+        Boss lastboss = new Boss();
+        int i;
+        for (i = 0; i <= 10; i++)
+        {
+
+
+            lastboss.Magic(5);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
